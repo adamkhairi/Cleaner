6b5df1a40dd8c97c2265a3dbc89edc92513410ba
adamkhairi <57996941+adamkhairi@users.noreply.github.com>
2020-12-17 21:57:00 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ccleaner
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClean_Click(object sender, RoutedEventArgs e)
+        {
+         
+        }
+
+        private void scan_Click(object sender, RoutedEventArgs e)
+        {
+
+            MessageBox.Show("Start scan");
+         
+
+            btnClean.IsEnabled = false;
+            btnHistory.IsEnabled = false;
+            btnUpdate.IsEnabled = false;
+
+            bigTitle.Content = "Scan in progress...";
+            scan.Visibility = Visibility.Hidden;
+            statisic.Visibility = Visibility.Hidden;
+            progress.Visibility = Visibility.Visible;
+
+
+            for (int i = 1; i <= 100; i++)
+            {
+                pbar.Value++;
+            }
+
+
+           
+
+
+        }
+
+        private void pbar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+        Duration duration = new Duration(TimeSpan.FromSeconds(2));
+            DoubleAnimation doubleAnimation = new DoubleAnimation(pbar.Value + 10, duration);
+            pbar.BeginAnimation(ProgressBar.ValueProperty, doubleAnimation);
+            //Thread.Sleep(10000);
+
+        }
+    }
+}
